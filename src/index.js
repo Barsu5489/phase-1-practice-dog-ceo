50dfcee652558f920748c9ced8d59440b90c3b00
barsu5489 <ebarsulai@gmail.com>
2022-12-27 11:05:44 +0300

    implemented filter using text color

--- src/index.js
+++ src/index.js
@@ -19,23 +19,59 @@ function breed(){
     .then(breeds=>{
         const breed = Object.keys(breeds.message)
         const dogBreeds = document.getElementById('dog-breeds')
+        const select = document.querySelector('#breed-dropdown')
+        const options = select.options;
+        // for(let i = 0; i < options.length; i++){
+        //         options[i].addEventListener('click', () =>{
+        //             console.log('a was clicked')
+        //         })
+            
+        // }
+        
+
+
+
     for(let i = 0; i < breed.length; i++){
-        console.log(breed[i])
+        
         const li = document.createElement('li')
         li.textContent = breed[i]
         dogBreeds.appendChild(li)
         li.addEventListener('click', ()=>{
             li.style.color = 'red'
-            console.log(li.textContent.split('').forEach(letter=>{
-                if(letter == 'a'){
-                    li.style.display = 'block'
-                    console.log('yes breddda')
-                }else{
-                    li.style.display = 'none'
-                    console.log('removed')
-                }
-            }))
         })
+            const filter_w = li.textContent.split('')
+            
+                const selectElement = document.querySelector('#breed-dropdown');
+                selectElement.addEventListener('change', function(event) {
+                    filter_w.forEach(letter=>{
+                    const selectValue = event.target.value
+                    if(selectValue === letter){
+                        console.log('present')
+                        li.style.color = 'red'
+                        
+                        console.log(li.textContent)
+                     }else if(selectValue !== letter){
+                        li.style.display -= 'none'
+                     }
+                  
+                     //else if (letter != selectValue){
+                    //     li.style.display = 'none'
+                    // }
+
+    
+                });
+            })
+        
+            // console.log(li.textContent.split('').forEach(letter=>{
+            //     if(letter == 'a'){
+            //         li.style.display = 'block'
+            //         console.log('yes breddda')
+            //     }else{
+            //         li.style.display = 'none'
+            //         console.log('removed')
+            //     }
+            // }))
+        
     }
        
     
